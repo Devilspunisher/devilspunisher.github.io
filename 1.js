7ca4224867dc1625f53f9ece03c2b986f1f1eeef
Maks <maksim.kyslov@gmail.com>
2018-08-28 02:14:06 +0300

    comite

---
+++ 1.js
@@ -0,0 +1,16 @@
+$('.sharetastic').sharetastic();
+{
+    sprite: "sharetastic.svg",
+    popup: true,
+    services: {
+      facebook: {
+        name: 'Facebook',
+        href: 'http://facebook.com',
+        icon: {
+          width: 10,
+          height: 19,
+          id: 'sharetastic-facebook'
+        }
+      }
+    }
+  }
